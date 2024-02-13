77b62039f0e08c30536889996196517cfeab8aac
Piotr Duda <dudapiotr90@gmail.com>
2024-02-13 08:02:01 +0100

    fix - missing return header

--- src/main/java/pl/dudi/repolistapp/dto/Branch.java
+++ src/main/java/pl/dudi/repolistapp/dto/Branch.java
@@ -1,6 +1,9 @@
 package pl.dudi.repolistapp.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
